25af3024fd6d3b95579bfca90eac7b4a3a404619
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2021-01-04 11:50:09 -0500

    Run isort and black

--- pytest_allclose/__init__.py
+++ pytest_allclose/__init__.py
@@ -5,9 +5,8 @@ pytest_allclose
 Pytest fixture extending Numpy's allclose function.
 """
 
-from .version import version as __version__
-
 from .plugin import report_rmses
+from .version import version as __version__
 
 __copyright__ = "2019-2019 pytest_plt contributors"
 __license__ = "MIT license"
