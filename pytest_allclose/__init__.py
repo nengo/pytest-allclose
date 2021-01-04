386711ec7358ba793550952c27ee950d3a939f36
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2021-01-04 11:50:38 -0500

    Update copyright dates to 2021

--- pytest_allclose/__init__.py
+++ pytest_allclose/__init__.py
@@ -8,5 +8,5 @@ Pytest fixture extending Numpy's allclose function.
 from .plugin import report_rmses
 from .version import version as __version__
 
-__copyright__ = "2019-2019 pytest_plt contributors"
+__copyright__ = "2019-2021 pytest_plt contributors"
 __license__ = "MIT license"
